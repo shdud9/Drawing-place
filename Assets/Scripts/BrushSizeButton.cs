98acad29fe2fa9631b61fee9246994433fcb5d5b
1L1S <ilusha.chap@gmail.com>
2026-05-07 19:04:50 +0200

    BrushSizeButton

---
+++ Assets/Scripts/BrushSizeButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class BrushSizeButton : MonoBehaviour
+    {
+        [SerializeField] private ColoringManager coloringManager;
+        
+        
+        public void increaseBrushSize()
+        {
+            if (coloringManager.brushSize < 100)
+            {
+                coloringManager.brushSize += 5;
+            }
+            
+        }
+
+        public void decreaseBrushSize()
+        {
+            if (coloringManager.brushSize > 10)
+            {
+                coloringManager.brushSize -= 5;
+            } 
+        }
+        
+    }
+}
